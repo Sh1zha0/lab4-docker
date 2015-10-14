29510964331ae5e29b09d694f5401535d1ac4723
Ubuntu <ubuntu@ip-172-30-0-102.eu-west-1.compute.internal>
2015-10-14 17:02:30 +0000

    Lab5

---
+++ lab4-docker/docker-lab4/my_application/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/")
+def index():
+    return "Index Page"
+
+@app.route("/user/<username>")
+def show_user(username):
+    return "User %s" % username
+
+@app.route("/post/<int:post_id>"):
+    return "Post %d" % post_id
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
